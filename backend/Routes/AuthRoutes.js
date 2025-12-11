677c707e20d7319f290222eaefba2d78d922451b
jainiahh <jainishpatel1782005@gmail.com>
2025-12-11 18:50:08 +0530

    changes

--- backend/Routes/AuthRoutes.js
+++ backend/Routes/AuthRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import router from express.Router();
+const router = express.Router();
 import { Signup, Login, getuser } from "../Controllers/AuthController";
-import verifyToken from "../Middleware/AuthMiddleware"
-import {signUpValidation,loginValidation,} from "../Middleware/AuthValidation";
+import verifyToken from "../Middleware/AuthMiddleware";
+import {
+  signUpValidation,
+  loginValidation,
+} from "../Middleware/AuthValidation";
 
 router.post("/signup", signUpValidation, Signup);
 router.post("/login", loginValidation, Login);
